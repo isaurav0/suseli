fe10af976f88fe1172629a0020cd819a04124152
Saurav Pathak <saurab.pathak.0@gmail.com>
2020-05-16 23:50:36 +0545

    build added

---
+++ server/build/precache-manifest.18a79771da220c503dfba76c2e63ae38.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9327401cedd9aa917a05731ef4f6bad3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "592afe6e8337070d8696",
+    "url": "./static/css/main.a5528372.chunk.css"
+  },
+  {
+    "revision": "bd7037bcd8b11062c6ae",
+    "url": "./static/js/2.1e98d009.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.1e98d009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "592afe6e8337070d8696",
+    "url": "./static/js/main.b3db88d2.chunk.js"
+  },
+  {
+    "revision": "b6d54f83cff1c81d887b",
+    "url": "./static/js/runtime-main.7e3b0c91.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0dca6e6468bacb9ffad15c957ea355ff",
+    "url": "./static/media/patrick.0dca6e64.woff"
+  },
+  {
+    "revision": "eeb4dad66cf2ff3ee47b7ac7906913a0",
+    "url": "./static/media/patrick.eeb4dad6.ttf"
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(morgan('combined'));
+app.use(express.static('build'));
 
 //run server 
 app.listen(3000, ()=>{
     console.log("server start on port 3000")
 })
 
-app.get("/songs", )
+app.get("*",(req, res)=>{
+	return res.sendFile(__dirname + '/build/index.html')
+})
