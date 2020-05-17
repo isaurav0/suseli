7b63af0f76ef8dd219f23b6e7fd42dea8008f369
Saurav Pathak <saurab.pathak.0@gmail.com>
2020-05-17 21:59:10 +0545

    built docker image

--- server/server.js
+++ server/server.js
@@ -8,9 +8,10 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(morgan('combined'));
 app.use(express.static('build'));
 
+const PORT = 8081 
 //run server 
-app.listen(8080, ()=>{
-    console.log("server start on port 3000")
+app.listen(PORT, ()=>{
+	console.log("server start on port "+ PORT)
 })
 
 app.get("*",(req, res)=>{
