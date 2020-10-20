5ac8caeb03a8216241aa83b80d43e22225dec773
Saurav Pathak <saurab.pathak.0@gmail.com>
2020-10-20 10:21:20 +0545

    streaming added

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-// app.use(morgan('combined'));
+app.use(morgan('combined'));
 app.use(express.static('build'));
 app.use(express.static('data'))
 
@@ -23,41 +23,33 @@ app.get("/",(req, res)=>{
 
 app.route('/api/auth', require('./routes/api/auth/auth'))
 
-app.get('/api/songs/stream', (req, res)=>{
-	const file = path.join(__dirname, 'data/audio/1.ogg')
+app.get('/api/songs/:id/stream', (req, res)=>{
+
+	const file = path.join(__dirname, `data/audio/${req.params["id"]}.mp3`)
+
+	if(!fs.existsSync(file))
+		return res.status(404).send('file not found')
+
 	const stat = fs.statSync(file);
 	const total = stat.size;
-	console.log("done")
-	if(!fs.existsSync(file))
-		return res.send("Error 404")
-	const range = req.headers.range;
-	console.log("Range to request made: ",range)
-	if(range){
-		const parts = range.replace(/bytes=/, '').split('-');
-		const partialStart = parts[0];
-		const partialEnd = parts[1];
+	const range = req.headers.range ? req.headers.range: '0';
+	const parts = range.replace(/bytes=/, '').split('-');
+	const partialStart = parts[0];
+	const partialEnd = parts[1];
 
-		const start = parseInt(partialStart, 10);
-		const end = partialEnd ? parseInt(partialEnd, 10) : total - 1;
-		// const chunksize = (end - start) + 1;
-		const chunksize = 0.5*1024*1024;
-		const rstream = fs.createReadStream(file, {start: start, end: end});
+	const start = parseInt(partialStart, 10);
+	const end = partialEnd ? parseInt(partialEnd, 10) : total - 1;
+	// const chunksize = (end - start) + 1;
+	const chunksize = 160*1024*8;
+	const rstream = fs.createReadStream(file, {start: start, end: end});
 
-		res.writeHead(206, {
-			'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
-			'Accept-Ranges': 'bytes', 'Content-Length': chunksize,
-			'Content-Type': 'audio/mpeg'
-		});
-		console.log('streaming')
-		rstream.pipe(res);
-	}	
-	else{
-		const rstream = fs.createReadStream(file);
-		res.writeHead(206, {
-			'Content-Type': 'audio/mpeg'
-		});
-		rstream.pipe(res)
-	}
+	res.writeHead(206, {
+		'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
+		'Accept-Ranges': 'bytes', 'Content-Length': chunksize,
+		'Content-Type': 'audio/mpeg'
+		// 'Content-Type': 'text/html'
+	});
+	rstream.pipe(res);
 })
 
 app.get('/api/songs/download', (req, res)=>{
